7f79e8419ad373dcf51cd69885695b95164383b9
Visser0 <113399663+Visser0@users.noreply.github.com>
2026-04-12 16:56:57 -0500

    foundational dialog box logic working
    Dialog box now appears and disappears correctly. Variables incorporated in order to make changes the text and image

--- Assets/Scripts/Dialog/NPCSystem.cs
+++ Assets/Scripts/Dialog/NPCSystem.cs
@@ -1,17 +1,31 @@
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class NPCSystem : MonoBehaviour
 {
     bool playerDetection = false;
+    public GameObject dialogBox;
+    public TextMeshProUGUI dialogText;
+    public Image portrait;
+    public string newDialog;
+
+    private void Start()
+    {
+        //make sure dialog box defaults to hidden
+        dialogBox.SetActive(false);
+    }
 
     // Update is called once per frame
     void Update()
     {
-        //if player is in the npc's space and hits dialog trigger (F)
-        if(playerDetection && Input.GetKeyDown(KeyCode.F))
+        //if player is in the npc's space and hits dialog trigger (Space)
+        if(playerDetection && Input.GetKeyDown(KeyCode.Space))
         {
-            print("Dialog started!");
+            print("Dialog started");
+            CreateDialogBox();
         }
+
     }
 
     //change detection bool to true if player is in the npc's space
@@ -27,5 +41,17 @@ public class NPCSystem : MonoBehaviour
     private void OnTriggerExit(Collider other)
     {
         playerDetection = false;
+    }
+
+    private void CreateDialogBox()
+    {
+        dialogText.text = newDialog;
+        dialogBox.SetActive(true);
+    }
+
+    public void CloseDialogBox()
+    {
+        dialogBox.SetActive(false);
+        print("Dialog Stopped");
     }
 }
