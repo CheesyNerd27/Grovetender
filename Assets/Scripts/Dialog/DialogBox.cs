ea0dba3ce9dc5ed70e1f22c8a1054d8d08b32226
Visser0 <113399663+Visser0@users.noreply.github.com>
2026-04-30 14:54:19 -0500

    JSON and Portrait Implementation
    Final commit for my work on the dialog box, good luck!
     --Jacinda :)

--- Assets/Scripts/Dialog/DialogBox.cs
+++ Assets/Scripts/Dialog/DialogBox.cs
@@ -2,15 +2,18 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 
-public class NPCSystem : MonoBehaviour
+public class DialogBox : MonoBehaviour
 {
     //checks for if the player is near the NPC
     bool playerDetection = false;
     
     //The changing parts of the dialog box
-    public GameObject dialogBox;
-    public TextMeshProUGUI dialogText;
-    public Image portrait;
+    public GameObject dialogBox;//the box itself (the parent object)
+    private TextMeshProUGUI dialogText; // the text element
+    private Image portrait;//the image element for the sprites
+    
+    //reference to our Dialog Keeper (the object holding the dialog getter script grabbing the data from the json)
+    private GetDialog DialogKeeper;
 
     //reference to the NPC we're talking to-> to decide what dialog ID we want
     GameObject NPCObject;
@@ -18,16 +21,17 @@ public class NPCSystem : MonoBehaviour
     //matches up to the specific dialog id we want in the json
     private int dialogID;
 
-    //reference to our Dialog Keeper
-    public SampleDialogStuff DialogKeeper;
-
-
-    private void Start()
+    private void Awake()
     {
-        //make sure dialog box defaults to hidden
+        //make sure dialog box starts as hidden
         dialogBox.SetActive(false);
 
-        //grab parent object-> refers to the NPC being talked to
+        //grab the relevant children objects of dialog box
+        dialogText = dialogBox.transform.Find("DialogText").GetComponent<TextMeshProUGUI>();
+        portrait = dialogBox.transform.Find("PortraitContainer/Portrait").GetComponent<Image>();
+        DialogKeeper = dialogBox.transform.Find("DialogKeeper").GetComponent<GetDialog>();
+
+        //grab parent object-> refers to the NPC being talked to-> used to decide which dialog is shown
         NPCObject = transform.parent.gameObject;
     }
 
@@ -37,6 +41,7 @@ public class NPCSystem : MonoBehaviour
         //if player is in the npc's space and hits dialog trigger (left click)
         if(playerDetection && Input.GetMouseButtonDown(0))
         {
+            //runs the dialog box creation function
             CreateDialogBox(NPCObject.name);
         }
 
@@ -59,6 +64,7 @@ public class NPCSystem : MonoBehaviour
 
     private void CreateDialogBox(string NPC)
     {
+        //decide which dialog (via dialog id) will be shown
         if (NPC == "SampleNPC")
         {
             dialogID = 0;
@@ -69,13 +75,13 @@ public class NPCSystem : MonoBehaviour
             dialogID = 1;
         }
 
-        //get new dialog for NPC
-        string[] newEntry = DialogKeeper.GrabDialog(dialogID);
 
+        //get new dialog for NPC using the dialog id
+        object[] newEntry = DialogKeeper.GrabDialog(dialogID);
 
         //change dialog box parts
-        dialogText.text = newEntry[0];
-        print(newEntry[1]); //portrait result standin
+        dialogText.text = (string)newEntry[0]; //text
+        portrait.sprite = (Sprite)newEntry[1]; //portrait image
 
         // activate/show the box
         dialogBox.SetActive(true);
--- Assets/Scripts/Dialog/GetDialog.cs
+++ Assets/Scripts/Dialog/GetDialog.cs
@@ -1,15 +1,17 @@
 using NUnit.Framework;
 using UnityEngine;
-using static SampleDialogStuff;
 
-public class SampleDialogStuff : MonoBehaviour
+public class GetDialog : MonoBehaviour
 {
+    //whats being packed to pass back to dialogbox
     string newDialog;
     string newPortrait;
-    private Dialog dialogItem;
 
     //json file
-    public TextAsset DialogText;
+    public TextAsset DialogFile;
+
+    //is the specific dialog item we picked via the dialog id
+    private Dialog dialogItem;
 
     //creates individual dialog item objects
     [System.Serializable]
@@ -31,20 +33,23 @@ public class SampleDialogStuff : MonoBehaviour
     //the overall list of dialog items from the json
     public DialogList myDialogList = new DialogList();
 
-    public string[] GrabDialog(int dialogID)
+    public object[] GrabDialog(int dialogID)
     {
         //grabs all the data out of the dialog json file (Future note: no this line can't be in the start function- it errors out despite how much it would make sense)
-        myDialogList = JsonUtility.FromJson<DialogList>(DialogText.text);
+        myDialogList = JsonUtility.FromJson<DialogList>(DialogFile.text);
 
         //grabs the target dialog object from ID
         dialogItem = myDialogList.dialog[dialogID];
 
         //pulls data to be displayed
         newDialog = dialogItem.text;
-        newPortrait = dialogItem.portrait;
+        newPortrait = dialogItem.portrait; //this is just the file path to the sprite image file
+
+        //finds the portrait sprite using the file path given
+        Sprite portraitSprite = Resources.Load<Sprite>(newPortrait);
 
         //consolidate data to be displayed
-        string[] newEntry = new string[] { newDialog, newPortrait };
+        object[] newEntry = new object[] { newDialog, portraitSprite };
 
         return newEntry;
     }
